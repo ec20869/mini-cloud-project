4eda6bf2c9bbe4f4dab044c03a85d08bc8fd4c85
Christa Dobson <75068207+ec20869@users.noreply.github.com>
2021-04-02 09:55:24 +0100

    Update sql_code.sql

--- sql_code.sql
+++ sql_code.sql
@@ -1,8 +1,5 @@
-CREATE TABLE music_data (
+CREATE TABLE musicapp.music_records (
 pk_record_id INT(6),
 name  VARCHAR(40) NOT NULL,
 albums VARCHAR(40) NOT NULL,
-title VARCHAR(50) NOT NULL,
-songs VARCHAR(50) NOT NULL,
-description VARCHAR(100) NOT NULL
-)
+);
